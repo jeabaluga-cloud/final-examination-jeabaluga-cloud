dd540f9319eaef8817ebf97452e51c3b90ce5f91
jeabaluga-cloud <jeabaluga@gmail.com>
2026-04-30 18:02:07 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 <?php
 $host = '127.0.0.1'; 
-$db   = 'cc111smsdb';  //sukatan yo dytoy jay nagan ti database yo
+$db   = 'balugadb';  //sukatan yo dytoy jay nagan ti database yo
 $user = 'root';        //
 $pass = '';            //
 $port = '3308';        //nu nagusar kayo sabali nga port sukatan yo metlang dytoy
